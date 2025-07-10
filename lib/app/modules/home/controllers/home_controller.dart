dd75fd260ab93e61c23c67443294e4b05c68ed09
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-07-10 13:07:27 +0600

    fix: home items to use named routes

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,22 @@
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
+import '../../../routes/app_pages.dart';
 import '../model/home_items.dart';
 
 class HomeController extends GetxController {
-
   @override
   void onInit() {
     super.onInit();
   }
 
-
   static const List<HomeItem> items = [
-    HomeItem('Global', FontAwesomeIcons.globe, '/global'),
-    HomeItem('Register', FontAwesomeIcons.userPlus, '/register'),
-    HomeItem('Login', FontAwesomeIcons.rightToBracket, '/login_patient'),
-    HomeItem('Hospital', FontAwesomeIcons.hospital, '/hospital'),
-    HomeItem('Doctor', FontAwesomeIcons.userDoctor, '/doctor'),
-    HomeItem('Ambulance', FontAwesomeIcons.truckMedical, '/ambulance'),
-    HomeItem('Patient', FontAwesomeIcons.addressCard, '/patient_details'),
+    HomeItem('Global', FontAwesomeIcons.globe, Routes.GLOBAL),
+    HomeItem('Register', FontAwesomeIcons.userPlus, Routes.REGISTER),
+    HomeItem('Login', FontAwesomeIcons.rightToBracket, Routes.REGISTER),
+    HomeItem('Hospital', FontAwesomeIcons.hospital, Routes.HOSPITAL),
+    HomeItem('Doctor', FontAwesomeIcons.userDoctor, Routes.DOCTOR),
+    HomeItem('Ambulance', FontAwesomeIcons.truckMedical, Routes.AMBULANCE),
+    HomeItem('Patient', FontAwesomeIcons.addressCard, Routes.PATIENT),
   ];
 }
