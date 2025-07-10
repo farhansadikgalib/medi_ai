79eec5902ffac27140050cff3d0884d147000743
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-07-10 14:24:05 +0600

    feat: refactor main function to include screen size initialization and system UI settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,59 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_value/shared_value.dart';
 
 import 'app/routes/app_pages.dart';
+import 'core/binding/initial_binding.dart';
+import 'core/constants/app_constants.dart';
+import 'core/style/app_colors.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await ScreenUtil.ensureScreenSize();
+  runApp(ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ]);
+        SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge,
+            overlays: [SystemUiOverlay.bottom]);
+
+        SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Brightness.dark));
+
+        return SharedValue.wrapApp(
+          GetMaterialApp(
+            title: "MediAI",
+            debugShowCheckedModeBanner: false,
+            initialRoute: AppPages.INITIAL,
+            initialBinding: InitialBinding(),
+            builder: EasyLoading.init(),
+            theme: ThemeData(
+              useMaterial3: false,
+              canvasColor: Colors.white,
+              primaryColor: AppColors.primaryColor,
+              textTheme: GoogleFonts.poppinsTextTheme(
+                Theme.of(context).textTheme,
+              ),
+            ),
+            defaultTransition: transition,
+            getPages: AppPages.routes,
+            enableLog: kDebugMode,
+          ),
+        );
+      }));
 }
+
