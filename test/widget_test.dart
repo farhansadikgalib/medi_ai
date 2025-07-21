cafcc349ab22ed4ec9af6538849297d3affd0204
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-07-21 10:18:15 +0600

    feat: add string concatenation test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  test('Sample test', () {
-    expect(1 + 1, 2);
-  });
-}
+    void main() {
+      test('Sample test', () {
+        expect(1 + 1, 2);
+      });
+
+      test('String concatenation', () {
+        expect('Hello ' + 'World', 'Hello World');
+      });
+    }
