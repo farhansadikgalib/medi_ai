d724dc911aaeaf4bd5ee7f00e3ae870b7c3159eb
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-07-22 11:51:55 +0600

    feat: enhance login functionality with email and phone support, update LoginResponse model

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:medi/app/core/config/app_config.dart';
+import 'package:medi/app/core/helper/app_widgets.dart';
+import 'package:medi/app/core/helper/print_log.dart';
 import 'package:medi/app/routes/app_pages.dart';
+
+import '../../../core/helper/auth_helper.dart';
+import '../../../remote/repository/auth/auth_repository.dart';
 
 class LoginController extends GetxController {
   final emailController = TextEditingController();
@@ -10,10 +17,46 @@ class LoginController extends GetxController {
   var tabIndex = 0.obs;
   final isPasswordVisible = false.obs;
 
-  void login() {
-    Get.offAllNamed(Routes.HOME);
+  @override
+  void onInit() {
+    super.onInit();
+    if (kDebugMode) {
+      emailController.text = 'farhansadikgalib@gmail.com';
+      passwordController.text = '12345678';
+    }
   }
 
+void login() async {
+    if (tabIndex.value == 0) {
+      // Email login
+      final email = emailController.text.trim();
+      final password = passwordController.text.trim();
+      final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+      if (!emailRegex.hasMatch(email)) {
+        AppWidgets().getSnackBar(title: "Error", message: "Invalid email format");
+        return;
+      }
+      var response = await AuthRepository().getSignIn(email, password);
+      if (response.success == true) {
+        await AuthHelper().setUserData(response, DateTime.now().toIso8601String());
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        AppWidgets().getSnackBar(title: "Info", message: response.message);
+      }
+    } else {
+      // Phone login
+      final phone = phoneController.text.trim();
+      final password = passwordController.text.trim();
+      printLog( countryCode+phone);
+    /*  var response = await AuthRepository().getSignInWithPhone(countryCode.value + phone, password);
+      if (response.success == true) {
+        await AuthHelper().setUserData(response, DateTime.now().toIso8601String());
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        AppWidgets().getSnackBar(title: "Info", message: response.message);
+      }*/
+    }
+  }
   @override
   void onClose() {
     emailController.dispose();
--- lib/app/remote/model/auth/login_response.dart
+++ lib/app/remote/model/auth/login_response.dart
@@ -1,46 +1,85 @@
 import 'dart:convert';
 
-LoginResponse loginResponseFromJson(String str) =>
-    LoginResponse.fromJson(json.decode(str));
+LoginResponse loginResponseFromJson(String str) => LoginResponse.fromJson(json.decode(str));
 
 String loginResponseToJson(LoginResponse data) => json.encode(data.toJson());
 
 class LoginResponse {
-  String? name;
-  String? email;
-  int? isAdmin;
-  int? isDriver;
-  String? token;
   String? message;
-  String? status;
+  bool? success;
+  Data? data;
+  String? token;
 
   LoginResponse({
-    this.name,
-    this.email,
-    this.token,
     this.message,
-    this.status,
-    this.isAdmin,
-    this.isDriver,
+    this.success,
+    this.data,
+    this.token,
   });
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
-    name: json["name"] ?? '',
-    email: json["email"] ?? '',
-    isAdmin: json["is_admin"] ?? 0,
-    isDriver: json["is_driver"] ?? 1,
-    token: json["token"] ?? '',
-    message: json["message"] ?? '',
-    status: json["status"] ?? '',
+    message: json["message"],
+    success: json["success"],
+    data: json["data"] == null ? null : Data.fromJson(json["data"]),
+    token: json["token"],
   );
 
   Map<String, dynamic> toJson() => {
-    "name": name,
-    "email": email,
-    "is_admin": isAdmin,
-    "is_driver": isDriver,
-    "token": token,
     "message": message,
-    "status": status,
+    "success": success,
+    "data": data?.toJson(),
+    "token": token,
+  };
+}
+
+class Data {
+  String? id;
+  String? email;
+  String? phoneNumber;
+  String? mediid;
+  String? owner;
+  String? password;
+  String? role;
+  String? verificationToken;
+  dynamic createdBy;
+  int? v;
+
+  Data({
+    this.id,
+    this.email,
+    this.phoneNumber,
+    this.mediid,
+    this.owner,
+    this.password,
+    this.role,
+    this.verificationToken,
+    this.createdBy,
+    this.v,
+  });
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+    id: json["_id"],
+    email: json["email"],
+    phoneNumber: json["phoneNumber"],
+    mediid: json["mediid"],
+    owner: json["owner"],
+    password: json["password"],
+    role: json["role"],
+    verificationToken: json["verificationToken"],
+    createdBy: json["createdBy"],
+    v: json["__v"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "_id": id,
+    "email": email,
+    "phoneNumber": phoneNumber,
+    "mediid": mediid,
+    "owner": owner,
+    "password": password,
+    "role": role,
+    "verificationToken": verificationToken,
+    "createdBy": createdBy,
+    "__v": v,
   };
 }
