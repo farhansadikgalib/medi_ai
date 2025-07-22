5e73985ee62bfa4d78d0394ad4f58f8e13f44357
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-07-22 11:51:35 +0600

    feat: implement environment variable support in AppConfig for dynamic configuration

--- lib/app/core/config/app_config.dart
+++ lib/app/core/config/app_config.dart
@@ -1,13 +1,14 @@
-class AppConfig {
-  static const currentVersion = '1.0.0';
-  static const bool https = true;
-  static const String publicFolder = "public";
-  static const String protocol = https ? "https://" : "http://";
-  static const String rawBaseUrl = "$protocol$domainPath";
-  static const String basePath = "$rawBaseUrl/$apiEndPath";
-  static const domainPath = "wa.acibd.com";
-  static const String apiEndPath = "api/driver-portal/api/";
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-  static const uploadImageBasePath = basePath;
-  static const releaseDate = '250525';
+class AppConfig {
+  static String get currentVersion => dotenv.get('CURRENT_VERSION', fallback: '');
+  static bool get https => dotenv.get('HTTPS', fallback: 'true') == 'true';
+  static String get publicFolder => dotenv.get('PUBLIC_FOLDER', fallback: 'public');
+  static String get protocol => https ? "https://" : "http://";
+  static String get domainPath => dotenv.get('DOMAIN_PATH', fallback: '');
+  static String get apiEndPath => dotenv.get('API_END_PATH', fallback: '');
+  static String get rawBaseUrl => "$protocol$domainPath";
+  static String get basePath => "$rawBaseUrl/$apiEndPath";
+  static String get imageBasePath => dotenv.get('IMAGE_BASE_PATH', fallback: '');
+  static String get releaseDate => dotenv.get('RELEASE_DATE', fallback: '');
 }
